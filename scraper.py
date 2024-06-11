53d4f866ba9e2714d56ac5778146b418d9bc1f5b
Julian-Lafond <jmlafond@ucsc.edu>
2024-06-11 14:22:41 -0700

    removed html tags

--- scraper.py
+++ scraper.py
@@ -6,8 +6,14 @@ def main():
     response = requests.get(url)
     
     soup = BeautifulSoup(response.content, "html.parser")
-    elements = soup.find_all(class_="comment") #find all the div elements that are called comment
-    print("Elements", len(elements))
+    elements = soup.find_all(class_="ind", indent = 0) #find all the div elements that are called comment which have an indent class and indent of 0
+    comments = [e.find_next(class_="comment") for e in elements] #iterate through elements list and find the next div that is called comment to get a list of all top level comments
+    
+    
+    for comment in comments:    ##prints all top level comments
+        comment_text = comment.get_text()   ##removes html tags
+        print(comment_text)
+    
         
     
 
