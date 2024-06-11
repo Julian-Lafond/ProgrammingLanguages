197176d2693f9cf5b8ba7d0fbe44fc5c90fcda21
Julian-Lafond <jmlafond@ucsc.edu>
2024-06-11 14:06:12 -0700

    fetched url:

--- scraper.py
+++ scraper.py
@@ -1,5 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
 def main():
-    print("Hello World")
+    url = "https://news.ycombinator.com/item?id=40563283"
+    response = requests.get(url)
+    
+    soup = BeautifulSoup(response.content, "html.parser")
+    elements = soup.find_all(class_="comment") #find all the div elements that are called comment
+    print("Elements", len(elements))
+        
+    
 
 
 if __name__ == "__main__":
